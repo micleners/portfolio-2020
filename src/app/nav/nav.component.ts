b6b637eb1787218e7472f219faff4df26bd5f667
mbeche <mb.johnsonbece@gmail.com>
2020-01-04 21:01:31 -0600

    Add nav highlight on home route

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  isHomeRoute: boolean;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.isHomeRoute = this.router.url === '/';
+    console.log(this.router.url);
+    console.log(this.isHomeRoute);
   }
-
 }
